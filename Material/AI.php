2086e5c16af2c64e7834202bc8df9bc055bac761
Naufal Khairul <naufalkr394@gmail.com>
2023-12-12 01:20:17 +0700

    added design and features in dashboard

---
+++ Material/AI.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/course.css">
+</head>
+
+<body>
+    <iframe width="560" height="315" src="https://www.youtube.com/watch?v=G2fqAlgmoPo&pp=ygULYWkgdHV0b3JpYWw%3D"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+
+    <iframe width="600" height="800"
+        src="..\pdfsources\Elementary Linear Algebra with Applications ninth edition.pdf" frameborder="0"></iframe>
+</body>
+
+</html>
